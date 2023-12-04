e93680871df4a62e213eb2591f2b2b76121dc86d
SegoML <masegoletsoko8@gmail.com>
2023-12-04 14:26:02 +0000

    first commit

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,69 +1,50 @@
 #!/usr/bin/python3
-"""Module with solution for the N-Queens challenge with backtracking"""
-import sys
+""" N queens puzzle, challenge of placing N non attacking queens
+on a NxN chessboard
+This program solves the N queens problem """
+
+from sys import argv
 
 
-def chessboard(pos, n):
-    """Function to print chessboard with appropriate positions of queens"""
-
-    board = []
-
-    for RANK in range(n):
-        for FILE in range(n):
-            if FILE == pos[RANK]:
-                board.append([RANK, FILE])
-
-    print(board)
-
-
-def safe_pos(pos, RANK, FILE, n):
-    """Function to determine safe a square to place a queen"""
-
-    if (pos[RANK] == FILE) or (pos[RANK] == FILE - RANK + n) or \
-            (pos[RANK] == RANK - n + FILE):
-        return True
-    return False
+def is_NQueen(cell: list) -> bool:
+    """ False if not N Queen, True if N Queen """
+    row_number = len(cell) - 1
+    difference = 0
+    for index in range(0, row_number):
+        difference = cell[index] - cell[row_number]
+        if difference < 0:
+            difference *= -1
+        if difference == 0 or difference == row_number - index:
+            return False
+    return True
 
 
-def get_safe_pos(board, rank, n):
-    """Function to get all safe positions to place queens with recursion"""
-
-    if rank == n:
-        chessboard(board, n)
+def solve_NQueens(dimension: int, row: int, cell: list, output: list):
+    """ Return result of N Queens recursively """
+    if row == dimension:
+        print(output)
     else:
-        for FILE in range(n):
-            safe = True
-
-            for RANK in range(rank):
-                if safe_pos(board, RANK, FILE, rank):
-                    safe = False
-
-            if safe:
-                board[rank] = FILE
-                get_safe_pos(board, rank + 1, n)
+        for column in range(0, dimension):
+            cell.append(column)
+            output.append([row, column])
+            if (is_NQueen(cell)):
+                solve_NQueens(dimension, row + 1, cell, output)
+            cell.pop()
+            output.pop()
 
 
-def create_board(n):
-    """Function to generate chessboard of n-size"""
-
-    return [0 * n for i in range(n)]
-
-
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
+if len(argv) != 2:
+    print('Usage: nqueens N')
     exit(1)
-
 try:
-    n = int(sys.argv[1])
-except Exception:
-    print("N must be a number")
+    N = int(argv[1])
+except BaseException:
+    print('N must be a number')
     exit(1)
-
-if (n < 4):
-    print("N must be at least 4")
+if N < 4:
+    print('N must be at least 4')
     exit(1)
-
-
-board = create_board(int(n))
-rank = 0
-get_safe_pos(board, rank, int(n))
+else:
+    output = []
+    cell = 0
+    solve_NQueens(int(N), cell, [], output)
